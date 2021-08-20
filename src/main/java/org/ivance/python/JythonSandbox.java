d38ba6fc967989318b33c4ba74c3f22c7050200c
RIvance <ivance@foxmail.com>
2021-08-21 02:10:36 +0800

    Add: PythonSandbox

--- src/main/java/org/ivance/python/JythonSandbox.java
+++
@@ -1,4 +0,0 @@
-package org.ivance.python;
-
-public class JythonSandbox {
-}
---
+++ src/main/java/org/ivance/python/PythonSandbox.java
@@ -0,0 +1,97 @@
+package org.ivance.python;
+
+import org.python.core.PyString;
+import org.python.core.PyUnicode;
+import org.python.util.PythonInterpreter;
+
+import java.io.ByteArrayOutputStream;
+import java.util.regex.Pattern;
+
+public class PythonSandbox {
+
+    private final PythonInterpreter interpreter = new PythonInterpreter();
+
+    private boolean autoClearOutput = true;
+    private final ByteArrayOutputStream outputBuffer = new ByteArrayOutputStream();
+
+    private String[] bannedKeywords;
+    private String initScriptTemplate;
+
+    private final String[] defaultBannedKeywords = {
+        "os", "sys", "pickle", "commands", "subprocess",    // modules
+        "eval", "exec", "open", "input", "compile", "dir",  // functions
+        "__builtins__", "__import__",
+    };
+
+    private final String defaultInitScriptTemplate = (
+        "banned_keywords = [${banned_keywords}]             \n" +
+        "for keyword in banned_keywords:                    \n" +
+        "    if __builtins__.__dict__.get(keyword) != None: \n" +
+        "        del __builtins__.__dict__[keyword]         \n"
+    );
+
+    private void init() {
+        StringBuilder bannedKeywordsBuilder = new StringBuilder();
+        for (String keyword : defaultBannedKeywords) {
+            bannedKeywordsBuilder.append("'").append(keyword).append("' ,");
+        }
+
+        String initScript = initScriptTemplate.replace("${banned_keywords}", bannedKeywordsBuilder.toString());
+        interpreter.eval(initScript);
+
+        interpreter.setOut(outputBuffer);
+        interpreter.setErr(outputBuffer);
+    }
+
+    public PythonSandbox() {
+        this.bannedKeywords = defaultBannedKeywords;
+        init();
+    }
+
+    public PythonSandbox(boolean allowSystemPermissions) {
+        if (!allowSystemPermissions) {
+            this.bannedKeywords = defaultBannedKeywords;
+            init();
+        }
+    }
+
+    public PythonSandbox(String initScriptTemplate) {
+        this.initScriptTemplate += initScriptTemplate;
+        init();
+    }
+
+    public PythonSandbox(String ... bannedKeywords) {
+        this.bannedKeywords = bannedKeywords;
+        init();
+    }
+
+    public void setAutoClearOutput(boolean doAutoClear) {
+        this.autoClearOutput = doAutoClear;
+    }
+
+    public void clearBuffer() {
+        outputBuffer.reset();
+    }
+
+    public String getBufferedOutput() {
+        return outputBuffer.toString();
+    }
+
+    public String eval(String rawScript) throws PythonScriptUnsafeException {
+        for (String keyword : bannedKeywords) {
+            String patten = "*[\\W]*" + keyword + "[\\W]**";
+            if (rawScript.matches(patten)) {
+                throw new PythonScriptUnsafeException();
+            }
+        }
+
+        PyString script = new PyUnicode(rawScript);
+        interpreter.eval(script);
+
+        String result = outputBuffer.toString();
+        if (autoClearOutput) {
+            clearBuffer();
+        }
+        return result.trim();
+    }
+}
