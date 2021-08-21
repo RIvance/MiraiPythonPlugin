dfe98d70296ada4abca126466fd8011d1a87caaf
RIvance <ivance@foxmail.com>
2021-08-21 19:51:23 +0800

    Test: ConditionTest, ResetTest, ...

--- src/test/java/org/ivance/PythonSandboxTest.java
+++ src/test/java/org/ivance/PythonSandboxTest.java
@@ -15,7 +15,7 @@ public class PythonSandboxTest {
     public void initTest() {
        try {
            PythonSandbox sandbox = new PythonSandbox("test_obj = 'test string'");
-           String result = sandbox.exec("print(test_obj)");
+           String result = sandbox.exec("print(test_obj)").trim();
            assertEquals(result, "test string");
        } catch (PythonScriptUnsafeException e) {
             fail();
@@ -39,6 +39,33 @@ public class PythonSandboxTest {
         interpreterField.setAccessible(true);
         PythonInterpreter interpreter = (PythonInterpreter) interpreterField.get(sandbox);
         interpreter.exec("print(__builtins__.__dict__.get('open') == None)");
-        assertEquals(sandbox.getBufferedOutput().trim(), "True");
+        assertEquals("True", sandbox.getBufferedOutput().trim());
+    }
+
+    @Test
+    public void tryExecTest() {
+        PythonSandbox sandbox = new PythonSandbox();
+        String result = sandbox.tryExec("import os");
+        assertEquals("Script contains keyword \"os\", which maybe unsafe", result);
+    }
+
+    @Test
+    public void conditionTest() {
+        PythonSandbox sandbox = new PythonSandbox();
+        sandbox.tryExec("print(x)");
+        assertEquals(PythonSandbox.ExecuteCondition.FAIL, sandbox.getLastExecuteCondition());
+    }
+
+    @Test
+    public void resetTest() {
+        PythonSandbox sandbox = new PythonSandbox();
+        sandbox.tryExec("test_var = 'test'");
+        sandbox.reset();
+        assertNotEquals(sandbox.tryExec("print(test_var)").trim(), "test");
+        try {
+            sandbox.exec("import os");
+            fail();
+        } catch (PythonScriptUnsafeException ignore) {
+        }
     }
 }
