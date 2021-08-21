36024bcaef6a0cc75aca4f01955cc501818b1720
RIvance <ivance@foxmail.com>
2021-08-21 18:00:22 +0800

    Add: UnitTests

--- src/test/java/org/ivance/PythonSandboxTest.java
+++ src/test/java/org/ivance/PythonSandboxTest.java
@@ -1,20 +1,44 @@
 package org.ivance;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import org.ivance.python.PythonSandbox;
+import org.ivance.python.PythonScriptUnsafeException;
 import org.junit.Test;
+import org.python.util.PythonInterpreter;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.lang.reflect.Field;
+
+public class PythonSandboxTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void initTest() {
+       try {
+           PythonSandbox sandbox = new PythonSandbox("test_obj = 'test string'");
+           String result = sandbox.exec("print(test_obj)");
+           assertEquals(result, "test string");
+       } catch (PythonScriptUnsafeException e) {
+            fail();
+       }
+    }
+
+    @Test
+    public void keywordTest() {
+        PythonSandbox sandbox = new PythonSandbox();
+        try {
+            sandbox.exec("import os");
+            fail();
+        } catch (PythonScriptUnsafeException ignored) {
+        }
+    }
+
+    @Test
+    public void bannedBuiltinsTest() throws NoSuchFieldException, IllegalAccessException {
+        PythonSandbox sandbox = new PythonSandbox();
+        Field interpreterField = PythonSandbox.class.getDeclaredField("interpreter");
+        interpreterField.setAccessible(true);
+        PythonInterpreter interpreter = (PythonInterpreter) interpreterField.get(sandbox);
+        interpreter.exec("print(__builtins__.__dict__.get('open') == None)");
+        assertEquals(sandbox.getBufferedOutput().trim(), "True");
     }
 }
